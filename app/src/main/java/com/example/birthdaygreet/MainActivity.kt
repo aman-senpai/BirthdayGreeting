41d5224a0c6ca8a494c40d40cd4f24c192216e76
aman-senpai <jemscollin1@gmail.com>
2022-11-11 21:55:42 +0530

    Birthday Greeting proejct phase 1

---
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.birthdaygreet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val name = nameInput.editableText.toString()
+        Toast.makeText(this, "name is $name!", Toast.LENGTH_LONG).show()
+    }
+}
